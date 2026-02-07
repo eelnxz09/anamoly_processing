e3607d5f0f61a82e8e517d1b26729558ec886e73
Neel Sonawane <neelsonawane2009@gmail.com>
2026-02-07 16:44:14 +0530

    Configure base path for GitHub Pages
    Set base path for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  // REQUIRED for GitHub Pages
+  base: '/anamoly_processing/',
+
+  // Local development only
   server: {
     port: 3000,
     proxy: {
